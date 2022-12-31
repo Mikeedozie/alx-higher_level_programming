ec95eee636a2d441781349f198d508a2adc386f2
Edozie <odigweedozie2@gmail.com>
2022-12-31 06:23:56 +0000

    corrected ascii code

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-alphabets = ""
-for letters in range(ord("a"), ord("z") + 1):
-	alphabets += (chr(letters))
-letter = "".join(alphabets)
-print("{}".format(letter))
+for  i in range(97, 123):
+    print("{:c}".format(i), end="")
